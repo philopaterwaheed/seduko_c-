ad54e48c8cd8c9773909358a320c743a12b10bbd
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2022-05-16 19:19:30 +0200

    Update moving.cpp

--- moving.cpp
+++ moving.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include<unistd.h>
-#include "array.hpp"
+#include <unistd.h>
+#include "drawing.hpp"
 #include <curses.h>
-struct point {
-int x=0,y=0; 
+struct point { // a struct to handle the point we at  
+int x=0,y=0,temp;
 
 };
 
@@ -13,17 +13,16 @@ int  press () {
 
 int press = getch();
     if (press!=ERR){
-    ungetch(press);
+    ungetch(press); // pushes the buttom to the head of the queue of buttons 
     return 1; 
-           
+
      }
     else 
         return 0;
 
 }
-
 void moving (){
-point matrix;
+point mat ; ///for matrix handle 
 initscr();
 std :: cout.flush();
 noecho();
@@ -34,10 +33,88 @@ while (true)
 {
 
     std :: cout.flush();
- 
-    if (press () ){
-        std ::  cout  << getch();    
-    refresh ();
+   mat.temp=nums[mat.y][mat.x];
+
+    if (press () ){ // i guess it checks if it was presses a buttom
+        nums [mat.y][mat.y]=0; 
+        int input = getch () ;
+
+        switch (input) {// to check the input to move 
+            case 119 ://w   
+  //                  std :: cout << " up " << std :: endl ;
+                        nums [mat.y][mat.x]=mat.temp ;
+                     mat.y--;
+                    if (mat.y == -1)
+                    mat.y = 8;
+                    mat.temp = nums [mat.y][mat.x];
+                        nums[mat.y][mat.x]=0;
+                    break; 
+            case 97 : //a 
+                   // std :: cout << " left " << std :: endl ;
+                        nums [mat.y][mat.x]=mat.temp ;
+                    mat.x--;
+                    if (mat.x==-1)
+                       mat.x = 8 ;
+                      mat.temp = nums [mat.y][mat.x];
+                        nums[mat.y][mat.x]=0;                     
+                        break;
+            case 115: //s 
+                   // std :: cout << "down " << std :: endl ;
+                        nums [mat.y][mat.x]=mat.temp ;
+                    mat.y++;
+                    if (mat.y==9)
+                        mat.y= 0; 
+                         mat.temp = nums [mat.y][mat.x];
+                        nums[mat.y][mat.x]=0;            
+                    break; 
+            case 100: //d 
+                  // std :: cout << " right " << std ::endl ;
+                        nums [mat.y][mat.x]=mat.temp ;
+                    mat.x++;
+                    if (mat.x == 9)
+                        mat.x=0; 
+                  mat.temp = nums [mat.y][mat.x];
+                        nums[mat.y][mat.x]=0;
+                    break;
+            case 49 : 
+                    nums [mat.y][mat.x]=1;
+                    break;
+            case 50 :
+                    nums [mat.y][mat.x]=2;
+                    break;
+            case 51 :
+                    nums [mat.y][mat.x]=3;
+                    break;
+            case 52 :
+                    nums [mat.y][mat.x]=4;
+                    break;
+            case 53 :
+                    nums [mat.y][mat.x]=5;
+                    break;
+            case 54 :
+                    nums [mat.y][mat.x]=6;
+                    break;
+            case 55 :
+                    nums [mat.y][mat.x]=7;
+                    break;
+            case 56 :
+                    nums [mat.y][mat.x]=8;
+                    break;
+            case 57 :
+                    nums [mat.y][mat.x]=9;
+                    break;
+            default :
+                    nums [mat.y][mat.x] = mat.temp; 
+
+
+                }
+       if (input<=119&&input>=97 ) // to return it the way it was                
+        {
+      nums [mat.y][mat.x]= mat.temp;
+//      std ::cout <<" wrks " ;
+}
+//      std :: cout << nums [mat.y][mat.x]<<std:: endl ;                
+        refresh ();
     }
     
     
@@ -46,11 +123,7 @@ while (true)
 
 }
 
-
-
-
-
-
-int main () {
-moving();
-}
+/*int main () {
+std :: cout << "works " ;
+moving ()  ;}
+*/
