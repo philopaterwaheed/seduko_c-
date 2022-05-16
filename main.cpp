a740b8b9aef1165c1ab96c8d7c53b170503c80bf
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2022-05-16 19:21:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
-#include "drawing.hpp"
 #include <thread>
-#include "array.hpp"
+//#include "array.hpp"
 //#include "moving.cpp"
 #include <iostream>
+void moving ();
+void draw ();
 int main (){
 std::thread d (draw);
-//std ::thread m (moving);
+std ::thread m (moving);
 
         d.join();
-//      m.join();  
+      m.join();  
 
      
     }
