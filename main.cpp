769ccf3f8033c18330ff323bdb882b25613b2ce8
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2022-05-17 18:01:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 void moving ();
 void draw ();
 int main (){
-std::thread d (draw);
-std ::thread m (moving);
+std::thread d (draw);   // prints 9*9 square and btween every line an ele from a 2d array
+std ::thread m (moving); // allow the user to move throw the arraay to change it and play
 
         d.join();
       m.join();  
