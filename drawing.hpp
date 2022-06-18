782a0c6fed36b9b65dfbe1c6fcd38a4343b988c6
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2022-06-19 01:17:23 +0200

    Update drawing.hpp

--- drawing.hpp
+++ drawing.hpp
@@ -1,40 +1,121 @@
-  GNU nano 5.4                     drawing.hpp                        M     
-//# pragma once
+//#include <iostream>
 #include <unistd.h>
-#include <iostream>
-#include "array.hpp"
-#include <curses.h>// just for the printw 
-void draw () {
-while (true){
-for (int i = 0 ; i <= 9; i ++)
-   
-    printw( "-");
+#include "drawing.hpp"
+#include <curses.h>
 
-   printw("\n");
-for (int i =0 ; i< 9 ;i++)
-        {
-            for (int j= 0; j<=18;j++)
-                {
- 
-                 if (j %2==0 )
-                       printw("|");
-                    else 
-                      printw("%d" ,nums[i][j/2]);
 
+int  press () {
+
+
+//std :: cout.flush();
+int press = getch();
+//std :: cout.flush();
+    if (press!=ERR){
+    ungetch(press); // pushes the buttom to the head of the queue of button>
+    return 1; 
+
+     }
+    else 
+        return 0;
+
+}
+void moving (){ 
+initscr();
+//std :: cout.flush();
+//noecho();
+nodelay(stdscr, TRUE);
+scrollok (stdscr,TRUE);
+mat.temp=nums[mat.y][mat.x];//saves the init value of [0][0] for moving  
+while (true)
+
+{
+
+//    std :: cout.flush();
+  
+
+    if (press () ){ // i guess it checks if it was presses a buttom
+//      nums [mat.y][mat.y]=0; 
+//      std :: cout.flush();
+        int input = getch () ;
+//std :: cout.flush();
+switch (input) {// to check the input to move 
+            case 119 ://w   
+  //                  std :: cout << " up\n "<<std::flush ;
+                        nums [mat.y][mat.x]=mat.temp ; // returns the past >
+                     --mat.y; // movea down 
+                    if (mat.y == -1) // checks if it's at the end of the bo>
+{                    mat.y = 8;}
+                    mat.temp = nums [mat.y][mat.x]; // saving the new squar>
+                        nums[mat.y][mat.x]=0; // makes the value of it 0 to>
+                    break; 
+            case 97 : //a 
+    //                std :: cout << " left ";// << std :: endl ;
+                        nums [mat.y][mat.x]=mat.temp ;
+                    mat.x--;
+                    if (mat.x==-1)
+                       mat.x = 8 ;
+                      mat.temp = nums [mat.y][mat.x];
+                        nums[mat.y][mat.x]=0;                     
+                        break;
+            case 115: //s 
+      //              std :: cout << "down ";// << std :: endl ;
+                        nums [mat.y][mat.x]=mat.temp ;
+                    mat.y++;
+                    if (mat.y==9)
+                        mat.y= 0; 
+                         mat.temp = nums [mat.y][mat.x];
+                        nums[mat.y][mat.x]=0;            
+                    break; 
+            case 100: //d 
+        //           std :: cout << " right " << std ::endl ;
+                        nums [mat.y][mat.x]=mat.temp ;
+                    mat.x++;
+                    if (mat.x == 9)
+                        mat.x=0; 
+                  mat.temp = nums [mat.y][mat.x];
+                        nums[mat.y][mat.x]=0;   
+                    break;
+            case 49 : 
+                    nums [mat.y][mat.x]=mat.temp=1;//change mat.temp too be>
+                    break;
+           case 50 :
+                    nums [mat.y][mat.x]=mat.temp=2;
+                    break;
+            case 51 :
+                    nums [mat.y][mat.x]=mat.temp=3;
+                    break;
+            case 52 :
+                    nums [mat.y][mat.x]=mat.temp=4;
+                    break;
+            case 53 :
+                    nums [mat.y][mat.x]=mat.temp=5;
+                    break;
+            case 54 :
+                    nums [mat.y][mat.x]=mat.temp=6;
+                    break;
+            case 55 :
+                    nums [mat.y][mat.x]=mat.temp=7;
+                    break;
+            case 56 :
+                    nums [mat.y][mat.x]=mat.temp=8;
+                    break;
+            case 57 :
+                    nums [mat.y][mat.x]=mat.temp=9;
+                    break;
+            default :
+                    nums [mat.y][mat.x] = mat.temp; 
 
 
                 }
-        
-printw("\n");
+       if (input<=119&&input>=97 ) // to return it the way it was          >
+        {
+//              nums [mat.y][mat.x]= mat.;
+//      std ::cout <<" wrks " ;
 }
-for (int i =0 ;i <=9;i++)
-    printw( "-"); 
-
-
-sleep(1);
-clear();
-
+//      std :: cout << nums [mat.y][mat.x];//<<std:: endl ;                
+//        refresh ();
+    }
+    
+    
 }
 }
-
-
